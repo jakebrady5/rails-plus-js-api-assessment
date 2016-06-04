3a6776930ebced704992d56c03828e6f3b70e508
jakebrady5 <jakebrady5@gmail.com>
2016-06-03 21:43:46 -0400

    Cleaned up work order controller

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -2,10 +2,6 @@ class WorkOrdersController < ApplicationController
 
   def complete
     WorkOrder.mark_complete_by_ids(params[:ids])
-    # params[:ids].each do |id|
-    #   order = WorkOrder.find_by(id: id)
-    #   order.update(status: 1)
-    # end
     redirect_to '/'
   end
 
